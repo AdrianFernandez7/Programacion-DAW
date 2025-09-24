d19c610eedaee2acb0b8c566b1769521b3a73200
AdrianFernandez7 <adrianfernandezparra07@gmail.com>
2025-09-24 14:21:33 +0200

    Actualizacion dia 24/09/25

--- fundamentos JAVA/src/App.java
+++ fundamentos JAVA/src/App.java
@@ -1,5 +1,45 @@
 public class App {
+    public static boolean logico=true;
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        String cadena="me llamo Adrian";
+        int edad=18;
+        double salario=14.568938948;
+        int x=5;   int y=7;   int z=10;
+        int xx=5,yy=7,zz=10;
+        //Defino una variable, la inicializo y la imprimo//
+        int variable=0;
+        System.out.println(variable);
+
+        variable=33;
+        System.out.println(variable);
+        int resultado=1/3;                //aqui estamos efectuando una división//
+        double resultado2=1/3.0;
+
+        System.out.println(resultado);
+        System.out.println(resultado2);
+
+        double a=(int)2.6; /*esto se usa para cuando vayamos a utilizar numeros reales y no enteros */
+        System.out.println(a);
+
+       System .out.println(logico);
+       logico=false;
+
+        int b='b';   //Un carácter se guarda en un enter según ASCII//
+        char c=98;
+        System.out.println(b+" "+c);
+
+        System.out.println(cadena);
+
+    }
+    //Definición de la función funcion//
+    //No va a sacar nigún resultado//
+
+/**programa para imprimir texto
+ * @author DomingoLópez Oller
+ * 
+ */
+
+    public static void imprimirTexto(){
+        int logico=3;
     }
 }
